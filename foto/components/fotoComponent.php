15f4cb55f0d17b2d80947b41d5ca43825968eae9
Cliente <Cliente@Cliente-PC>
2015-11-12 13:40:17 -0200

    drawfoto

--- foto/components/fotoComponent.php
+++ foto/components/fotoComponent.php
@@ -4,7 +4,8 @@ class fotoComponent extends classes\Component\Component{
     
     private $url_actions;
     private $enablegurl;
-    private $flush = "";
+    private $flush   = "";
+    private $nonimgs = array('pdf','doc','docx','xls');
     public function __construct() {
         $res               = classes\Classes\Registered::getResourceLocationUrl('upload');
         $this->url_actions = $res . "/src/lib/actions";
@@ -29,11 +30,12 @@ class fotoComponent extends classes\Component\Component{
 
     
             public function DrawPicture($foto, $print, $capa = ""){
+                if(!is_array($foto) || empty($foto)){return "";}
                 $url   = $this->getUrl($foto, "medium");
                 $url2  = $this->getUrl($foto, "");
                 $id    = $foto['cod_foto'];
                 $class = ($id == $capa)?"is_capa":"";
-                $album = isset($foto['cod_album'])?"data-lightbox='{$foto['cod_album']}'":"";
+                $album = (isset($foto['cod_album']) && $this->nonImage == false)?"data-lightbox='{$foto['cod_album']}'":"";
                 $var   = "
                 <div class='img_container $class col-xs-6 col-sm-4 col-md-3 col-lg-2'>
                     <a href='$url2' target='_BLANK' id='foto_$id' class='img album_capa col-xs-12' $album>
@@ -51,18 +53,38 @@ class fotoComponent extends classes\Component\Component{
                 if($print) {echo $var;}
                 else {$this->flush .= $var; return $var;}
             }
-            
+                    private $nonImage = false;
                     public function getUrl($foto, $sufix = "min"){
                         if(!in_array($sufix, array('min', 'max', 'medium', ''))) {$sufix = 'min';}
-                        $sufix = ($sufix != "")? "_$sufix": $sufix;
-                        $url = URL_IMAGENS . $foto['url'] . "$sufix." . $foto['ext'];
+                        if(in_array($foto['ext'], $this->nonimgs)){
+                            $this->nonImage = true;
+                            $url = URL_IMAGENS . $foto['url'];
+                            $this->getUrlNonImage($url, $sufix, $foto);
+                        }
+                        else{
+                            $this->nonImage = false;
+                            $sufix = ($sufix != "")? "_$sufix": $sufix;
+                            $url = URL_IMAGENS . $foto['url'] . "$sufix." . $foto['ext'];
+                        }
+                        
                         getTrueUrl($url);
                         return $url;
                     }
+                    
+                            private function getUrlNonImage(&$url, $sufix, $foto){
+                                if($sufix == ""){return;}
+                                $url = "";
+                                if(defined('CURRENT_TEMPLATE')){
+                                    $url = classes\Classes\Registered::getTemplateLocationUrl(CURRENT_TEMPLATE);
+                                }
+                                if($url == ""){
+                                    $url = classes\Classes\Registered::getTemplateLocationUrl('ppdh');
+                                }
+                                if($url !== "") {$url .= "/img/filetypes/{$foto['ext']}_$sufix.png";}
+                            }
     
     public function enableGetUrl(){
         $this->enablegurl = true;
     }
-    
     
 }
